8acaafa0941cf0ed0df63106533b4c7b77c20c72
Sohar <zarazgr8@gmail.com>
2020-09-29 13:27:36 +0400

    test

---
+++ Class 5/list_learn.py
@@ -0,0 +1,27 @@
+# my_list= ["sharpner","pencil","pen","eraser"]
+# second_ele=my_list[4-1]
+# print(second_ele)
+#
+# len_list=len(my_list)
+# print(len_list)
+#
+# last_elem=my_list[len_list-1]
+# print(last_elem)
+
+thisList=["orange","banana",3,4,"eraser","zara","sohar"]
+len_list=len(thisList)
+last_elem=thisList[len_list-1]
+print(last_elem)
+
+#append
+thisList.append("desk")
+
+thisList.append("burj khalifa")
+
+
+#remove
+thisList.remove("banana")
+
+
+thisList.pop()
+print(thisList)
